a96a1a9b0f1ce17ff5e8eced90e25f0e4425ccd2
Joan Grau <jgrau@peersyst.com>
2023-06-23 09:09:45 +0200

    remove new default metadata function

--- examples/mb-fungible-token/src/lib.rs
+++ examples/mb-fungible-token/src/lib.rs
@@ -35,8 +35,6 @@ pub struct Contract {
     owner_id: AccountId,
 }
 
-const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
-
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
     FungibleToken,
@@ -44,27 +42,7 @@ enum StorageKey {
 }
 
 #[near_bindgen]
-impl Contract {
-    /// Initializes the contract with the given total supply owned by the given `owner_id` with
-    /// default metadata (for example purposes only).
-    #[init]
-    pub fn new_default_meta(owner_id: AccountId, total_supply: U128) -> Self {
-        Self::new(
-            owner_id,
-            total_supply,
-            FungibleTokenMetadata {
-                spec: FT_METADATA_SPEC.to_string(),
-                name: "Test Token Default".to_string(),
-                symbol: "TTD".to_string(),
-                icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
-                reference: None,
-                reference_hash: None,
-                decimals: 24,
-            },
-        )
-    }
-
-    /// Initializes the contract with the given total supply owned by the given `owner_id` with
+impl Contract {    /// Initializes the contract with the given total supply owned by the given `owner_id` with
     /// default metadata (for example purposes only).
     #[init]
     pub fn new_name_symbol(owner_id: AccountId, total_supply: U128, name: String, symbol: String) -> Self {
@@ -75,7 +53,7 @@ impl Contract {
                 spec: FT_METADATA_SPEC.to_string(),
                 name: name,
                 symbol: symbol,
-                icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
+                icon: None,
                 reference: None,
                 reference_hash: None,
                 decimals: 24,
@@ -106,7 +84,7 @@ impl Contract {
         this
     }
 
-    /// Mints new tokens and sends it to receiver_id account
+    /// Mints new tokens and sends it to receiver_id account (only owner)
     pub fn ft_mint(&mut self, receiver_id: AccountId, amount: U128, memo: Option<String>) {
         let escrow_account_id = env::predecessor_account_id();
         require!(escrow_account_id == self.owner_id, "Only owner can mint");
@@ -119,7 +97,7 @@ impl Contract {
         }.emit();
     }
 
-    /// Burns tokens
+    /// Burns tokens from account (only owner)
     pub fn ft_burn(&mut self, account_id: AccountId, amount: U128, memo: Option<String>) {
         let escrow_account_id = env::predecessor_account_id();
         require!(escrow_account_id == self.owner_id, "Only owner can burn");
